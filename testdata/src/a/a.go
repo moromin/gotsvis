682dfc36eacb5d5329acc14fc809426e350d9648
moromin <34294957+moromin@users.noreply.github.com>
2022-03-17 22:17:40 +0900

    test: bug? case

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,31 +1,41 @@
 package a
 
 type MyInt int
+type MyMyInt MyInt
 type I interface {
 	string | ~int | bool
 }
 
-type A interface {
-}
-
-type B interface {
-	string | int | float64
-	Equal() bool
-}
-
-type D interface {
-	string | ~int
-	MyInt
-}
-
-// type E interface {
-// 	int | string
-// 	float64
+// error: empty
+// type A interface {
 // }
 
+// error: contains method
+// type B interface {
+// 	string | int | float64
+// 	Equal() bool
+// }
+
+// type D interface {
+// 	string | ~int
+// 	MyInt
+// }
+
+type E interface {
+	int | string
+	float64
+}
+
+// Bug?
+// If a single type with ~ is included, it is interpreted as *types.Union.
+// With the current go/types package, the following two are impossible to distinguish.
+// So, it is necessary to traverse ASTs using *ast.BinaryExpr, etc. in the go/ast package.
 // type F interface {
 // 	int | string
-// 	float64
+// 	~float64
+// }
+// type F interface {
+// 	int | string | ~float64
 // }
 
 // type G interface {
