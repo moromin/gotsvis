0f4efee64a399ad04600e03e4fa750d9e09de72f
moromin <34294957+moromin@users.noreply.github.com>
2022-03-17 17:36:47 +0900

    test: modify testdata

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -7,32 +7,31 @@ type C interface { // want "[~string int]"
 	~string | int
 }
 
-// type B interface { // want "[string int float64]"
-// 	string | int | float64
-// }
+type B interface { // want "[string int float64]"
+	string | int | float64
+}
 
-// type D interface { // want "[string int float64 bool]"
-// 	string | int | float64 | bool
-// }
+type D interface { // want "[string int float64 bool]"
+	string | int | float64 | bool
+}
 
-// type E interface { // want "[int string float4]"
-// 	int | string
-// 	float64
-// }
+type E interface { // want "[int string float4]"
+	int | string
+	float64
+}
 
-// type F interface { // want "[int string float64]"
-// 	int | string
-// 	float64
-// }
+type F interface { // want "[int string float64]"
+	int | string
+	float64
+}
 
-// type G interface {
-// 	string | float64
-// 	~int
-// 	Equal() bool
-// }
+type G interface {
+	string | float64
+	~int
+}
 
-// type MyInt int
-// type H interface {
-// 	~int | string
-// 	int | ~string
-// }
+type MyInt int
+type H interface {
+	~int | ~string
+	MyInt
+}
