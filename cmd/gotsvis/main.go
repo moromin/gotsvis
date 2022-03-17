147457e66165a8c9525ce55f81632e5cc93acfcf
moromin <34294957+moromin@users.noreply.github.com>
2022-03-17 20:13:50 +0900

    refactor: remove comment out in main.go

--- cmd/gotsvis/main.go
+++ cmd/gotsvis/main.go
@@ -40,17 +40,6 @@ func run() error {
 		}
 	}
 
-	// AST dump
-	// for i, _ := range pkgs {
-	// 	fset := token.NewFileSet()
-	// 	f, err := parser.ParseFile(fset, os.Args[i+1], nil, 0)
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 		os.Exit(1)
-	// 	}
-	// 	ast.Print(fset, f)
-	// }
-
 	return nil
 }
 
@@ -69,7 +58,6 @@ func analyze(pkg *packages.Package) error {
 		case *ast.TypeSpec:
 			if n.Type != nil {
 				title = n.Name.Name
-				// fmt.Printf("Type set: %s\n", n.Name.Name)
 			}
 		case *ast.InterfaceType:
 			if n.Methods == nil {
@@ -79,26 +67,6 @@ func analyze(pkg *packages.Package) error {
 			for _, e := range n.Methods.List {
 				typ := pkg.TypesInfo.TypeOf(e.Type)
 				res = append(res, typ)
-				// fmt.Printf("\t%[1]T %[1]v\n", typ.String())
-				// fmt.Println()
-				// set := make([]types.Type, 0)
-				// switch typ := typ.(type) {
-				// case *types.Union:
-				// fmt.Printf("\t%[1]T %[1]v\n", typ.String())
-				// for i := 0; i < typ.Len(); i++ {
-				// set = append(set, typ.Term(i))
-				// term := typ.Term(i)
-				// fmt.Printf("\t\t%[1]T %[1]v\n", typ.Term(i))
-				// if term.Tilde() {
-				// } else {
-				// 	fmt.Printf("\t\t%[1]T %[1]v\n", term.Type())
-				// }
-				// }
-				// res = append(res, set)
-				// default:
-				// res = append(res, []string{typ.String()})
-				// fmt.Printf("\t%[1]T %[1]v\n", typ)
-				// }
 			}
 			gotsvis.Venn(title, res)
 		}
